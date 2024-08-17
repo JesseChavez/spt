fdded3eeb9072419d9dd206f9553f54bfa587448
Jesse Chavez <jesse.chavez.r@gmail.com>
2024-08-17 11:21:52 +1000

    refactor FetchEnv function

---
+++ spt.go
@@ -0,0 +1,14 @@
+package spt
+
+import "os"
+
+// FetchEnv returns the passed default value to a environment variable.
+func FetchEnv(envVar string, defaultValue string) string {
+	envValue, ok := os.LookupEnv(envVar)
+
+	if ok {
+		envValue = defaultValue
+	}
+
+	return envValue
+}
